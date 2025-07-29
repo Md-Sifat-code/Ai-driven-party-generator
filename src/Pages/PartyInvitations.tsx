8e5295bba065866a0ee738456da4d16d7d88da82
AnisulHaqueNiloy <niloy2931@gmail.com>
2025-07-30 01:32:39 +0600

    routes are added

---
+++ src/Pages/PartyInvitations.tsx
@@ -0,0 +1,6 @@
+
+export default function PartyInvitations() {
+    return (
+        <div>PartyInvitations</div>
+    )
+}
---
+++ src/Pages/ProviderDetails.tsx
@@ -0,0 +1,6 @@
+
+export default function ProviderDetails() {
+    return (
+        <div>ProviderDetails</div>
+    )
+}
